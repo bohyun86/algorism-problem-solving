291f6e42f8a589d5fa5872998ba750af31bef1e0
bohyun <bohyun86@kakao.com>
2024-06-27 20:51:38 +0900

    fibonacci

---
+++ DynamicProgrammingApproach/Fibonacci.java
@@ -0,0 +1,34 @@
+package DynamicProgrammingApproach;
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tryNum = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < tryNum; i++) {
+            int num = Integer.parseInt(sc.nextLine());
+            fibonacci(num);
+        }
+
+    }
+
+    public static void fibonacci(int num) {
+        if (num == 0) {
+            System.out.println("1 0");
+            return;
+        } else if (num == 1) {
+            System.out.println("0 1");
+            return;
+        }
+        int[] fib = new int[num + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int i = 2; i <= num; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        System.out.println(fib[num - 1] + " " + fib[num]);
+    }
+}
