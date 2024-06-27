74a57fb6ff62eed904554e6e68d83afed473cb23
bohyun <bohyun86@kakao.com>
2024-06-27 20:55:22 +0900

    fibonacci

--- DynamicProgrammingApproach/Fibonacci.java
+++ DynamicProgrammingApproach/Fibonacci.java
@@ -1,6 +1,14 @@
 package DynamicProgrammingApproach;
 import java.util.Scanner;
 
+/**
+ * Fibonacci
+ * 재귀적인 방법으로 피보나치 수열을 구했을 경우, 0과 1이 몇 번 호출되는지 구하는 문제
+ * 재귀적인 방식으로 접근하면 비효율적이며 시간복잡도가 O(2^n)이기 때문에 동적 프로그래밍을 사용하여 풀어야 한다.
+ * 동적 프로그래밍을 사용하면 시간복잡도가 O(n)이 된다.
+ * 0과 1일 호출되는 횟수는 각각 피보나치 수열의 n-1번째와 n번째 수이다.
+ */
+
 public class Fibonacci {
 
     public static void main(String[] args) {
